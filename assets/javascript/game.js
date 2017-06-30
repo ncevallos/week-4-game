72001368a5c0c836c8c6bdb58da70e55b6fafe0c
ncevallos <nmcevall@gmail.com>
2017-06-30 10:57:51 -0400

    game nearly finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,46 +5,39 @@ var playerScore = 0;
 var goalScore = 0;
 var crystalValues = [0, 0, 0, 0];
 initializeCrystalValues();
-//initialize player's score, which will be zero but will be dynamcially updated as the player clicks on crystals
 
-//initialize variable for player's goal score to achieve, between 19 -120
-//perhaps initialize as a function that is called on start of game and can be called later when a players wins/loses so that it resets.
+//the function below initializes the values for the crystals as well as for the goal score and re-initializes the player's score to zero. This function is first called when the page is loaded, but can later be called when the player wins or loses.
 function initializeCrystalValues(){
 	goalScore  = [Math.floor(Math.random() * (120-19)) + 19];
 	playerScore = 0;
 	for(i=0; i<4; i++){
 		crystalValues[i] = [Math.floor(Math.random() * (12-1)) + 1];
 
-		console.log("new goalScore is " + goalScore);
 	}
 }
-for(i=0; i<4; i++){
-		console.log("spot at: " +i+ " = " +crystalValues[i]);
-	}
-//after each click on a crystal the game must run through a for loop determining whether the players current score is less than, equal to, or greater than the goal score and respond correctly to such.
+
+//the code below tells the program what to do when the image of each crystal is clicked. the check score function is called after each click to see if the changes incurred with the clicked have caused the player to win or lose.
 
  $(document).ready(function() {
  $("#blueCrystal0").on("click", function() {
     playerScore = playerScore+parseInt(crystalValues[0]);
-    console.log(playerScore);
     checkScore();
  });
  $("#greenCrystal1").on("click", function() {
     playerScore = playerScore+parseInt(crystalValues[1]);
-    console.log(playerScore);
     checkScore();
  });
  $("#purpleCrystal2").on("click", function() {
     playerScore = playerScore+parseInt(crystalValues[2]);
-    console.log(playerScore);
     checkScore();
  });
  $("#redCrystal3").on("click", function() {
     playerScore = playerScore+parseInt(crystalValues[3]);
-    console.log(playerScore);
     checkScore();
  });
  });
+
+ //function below checks the player's score against the goal score and determines if a player has win, lost, or is to keep playing.
 function checkScore(){
 	 	if(playerScore==goalScore){
 		alert("You WIN!");
@@ -59,28 +52,29 @@ function checkScore(){
 	else{
 	}
 }
-//whenever the player wins or loses run a reset functions that resets and starts all the values to the game.
 
+
+//the code below refreshes each time the mouse is clicked, and displays data like the player's current score, goal score, and updates their wins and losses.
 document.onclick = function(event) {
 
-	//change from on key up to when the page loads.
+
         var html =
           "<p>Goal Score: " + goalScore + "</p>";
 
-        // The html that displays the losses, wins, and guesses left.
+        // The html that displays the goal score.
         document.querySelector("#goalScore").innerHTML = html;
 
         var html2 =
           "<p>Wins: " + wins + "<br>" +
           "Losses: " + losses + "</p>";
 
-        // The html that displays the losses, wins, and guesses left.
+        // The html that displays the losses, and wins.
         document.querySelector("#gameText").innerHTML = html2;
 
         var html3 =
           "<p>" + playerScore + "</p>";
 
-        // The html that displays the losses, wins, and guesses left.
+        // The html that displays the player's score.
         document.querySelector("#playerScore").innerHTML = html3;
 
 
